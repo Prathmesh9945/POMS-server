3b5775035069aaedddf7558f59023240a9b19f48
Prathmesh895 <125139465+Prathmesh895@users.noreply.github.com>
2025-05-07 15:29:24 +0530

    new code

--- Routes/Product.js
+++ Routes/Product.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { ProductCtr, getProduct } = require('../Controllers/ProducyCtr')
+const { ProductCtr, getProduct, getProductByID, updateProductByID } = require('../Controllers/ProducyCtr')
 
 
 router.post('/', ProductCtr);
 router.get('/', getProduct)
+router.get('/:id', getProductByID)
+router.patch('/:id', updateProductByID)
 
 module.exports = router;
